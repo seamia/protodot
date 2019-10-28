87f37f572bf998f4567ccc5da6e9e803a1be9f31
Elliot <elliot@identitii.com>
2019-10-28 16:14:30 +1100

    add go.mod and go generate command  to update static assets

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/seamia/protodot
+
+go 1.12
+
+require (
+	github.com/emicklei/proto v1.6.8
+	github.com/seamia/tools v0.0.0-20190210011128-a37c92378810
+)
--- main.go
+++ main.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:generate go run github.com/seamia/tools/assets/cmd/assets -src assets.txt -root .
+
 package main
 
 import (
